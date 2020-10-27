7d7dbb45c42fe3dacfa6bf281b929fec628f649f
Norris Mei <norrismei3@gmail.com>
2020-10-27 20:38:31 +0000

    Created Card class

--- model.py
+++ model.py
@@ -16,6 +16,30 @@ def connect_to_db(flask_app, db_uri='postgresql:///taboo', echo=False):
     print('Connected to the db!')
 
 
+class Card(db.Model):
+    """A Taboo card"""
+
+    __tablename__ = "cards"
+
+    id = db.Column(db.Integer,
+                   autoincrement=True,
+                   primary_key=True)
+    room_id = db.Column(db.String)
+    keyword = db.Column(db.String, nullable=False)
+    buzzword_1 = db.Column(db.String)
+    buzzword_2 = db.Column(db.String)
+    buzzword_3 = db.Column(db.String)
+    buzzword_4 = db.Column(db.String)
+    buzzword_5 = db.Column(db.String)
+    player_created = db.Column(db.Boolean, default=False, nullable=False)
+
+
+    def __repr__(self):
+        """Show human-readable info about card"""
+
+        return f"<Card id={self.id} keyword={self.keyword}>"
+
+
 if __name__ == '__main__':
     from server import app
 
